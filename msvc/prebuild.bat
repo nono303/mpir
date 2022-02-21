9255ddb09b5dcb4e6a8633a778fa859a540f4435
nono303 <git@nono303.net>
2022-02-21 17:49:44 +0100

    fix 'outdir' creation for YASM (it doesn't create it) with %4 > vcxproj:$(Configuration) add verbose for gmp-mparam.h copy

--- msvc/prebuild.bat
+++ msvc/prebuild.bat
@@ -2,23 +2,35 @@
 rem %1 = mpn directory (generic, x86\... or x86_64\...) 
 rem %2 = platform (win32 or x64)
 rem %3 = MSVC version number (e.g. 14)
+rem %4 = Configuration (e.g. Release | Debug)
 
 if /i "%2" EQU "win32" ((set platform=win32) & (set bdir=x86w\)) else ((set platform=x64) & (set bdir=x86_64w\))
 set sdir=
-if /i "%1" EQU "gc" ((set sdir=generic) & (set bdir=generic)) else (set sdir=%bdir%%1)
-if not exist ..\mpn\%sdir% (call :seterr & echo ERROR: %1 is not supported & exit /b %errorlevel%)
+if /i "%1" EQU "gc" ((set sdir=generic\) & (set bdir=generic)) else (set sdir=%bdir%%1\)
+if not exist ..\..\..\mpn\%sdir% (call :seterr & echo ERROR: %1 is not supported & exit /b %errorlevel%)
 
 echo building MPIR for %1 (%platform%) from directory mpn\%sdir%
 
-set cdir=vs%3\cdata\mpn\%sdir%\
-set sdir=..\mpn\%sdir%\
-set bdir=..\mpn\%bdir%\
+REM fix out dir for YASM
+set configuration=%4
+if not exist %platform%\%configuration%\mpn\. md %platform%\%configuration%\mpn
+echo creating outdir for yasm '%platform%\%configuration%\mpn'
+cd /D ..\..
+
+set cdir=vs%3\cdata\mpn\%sdir%
+set sdir=..\mpn\%sdir%
+set bdir=..\mpn\%bdir%
 
 call gen_mpir_h %platform%
 call gen_config_h %cdir%
 
-if exist %sdir%\gmp-mparam.h (call out_copy_rename %sdir%\gmp-mparam.h ..\ gmp-mparam.h) else (
-    call out_copy_rename %bdir%\gmp-mparam.h ..\ gmp-mparam.h)
+if exist %sdir%gmp-mparam.h (
+	call out_copy_rename %sdir%gmp-mparam.h ..\ gmp-mparam.h
+	echo use %sdir%gmp-mparam.h
+) else (
+    call out_copy_rename %bdir%gmp-mparam.h ..\ gmp-mparam.h
+    echo use %bdir%gmp-mparam.h
+)
 
 type ..\longlong_pre.h >tmp.h
 type %bdir%\longlong_inc.h >>tmp.h
